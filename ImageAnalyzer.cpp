d2a46f6e3a472118e5007ef2442bcbbf056c2e10
Kate Wells <k8wells@tamu.edu>
2012-11-05 12:13:12 -0600

    Image analyzer working; no Mongo integration yet

--- ImageAnalyzer.cpp
+++ ImageAnalyzer.cpp
@@ -27,6 +27,7 @@ int ImageAnalyzer::ReadImage(char _filename[]) {
 	
 	pixels.clear();
 	
+	printf("OPENING IMAGE %s\n", _filename);
 	FILE *image = fopen(_filename, "rb");
 	
 	if (!image)
@@ -36,15 +37,21 @@ int ImageAnalyzer::ReadImage(char _filename[]) {
 	}
 	
 	// error handler
-	cinfo.err = jpeg_std_err(&jerr);
+	cinfo.err = jpeg_std_error(&jerr);
+	
+	puts("DECOMPRESSING IMAGE");
 	// decompression, read JPEG header
 	jpeg_create_decompress(&cinfo);
 	jpeg_stdio_src(&cinfo, image);
-	// get image inf
+	puts("GETTING IMAGE INFO");
+	// get image info
 	jpeg_read_header(&cinfo, true);
 	
+	puts("GETTING WIDTH");
 	width = cinfo.image_width;
+	puts("GETTING HEIGHT");
 	height = cinfo.image_height;
+	puts("GETTING COMPONENTS");
 	components = cinfo.num_components;
 	
 	printf("IMAGE INFO OBTAINED FOR %s\n", _filename);
@@ -52,17 +59,78 @@ int ImageAnalyzer::ReadImage(char _filename[]) {
 	
 	// start reading image	
 	JSAMPROW row_pointer[1];
+	for (int y = 0; y < height; y++)
+	{
+		vector<int> temp;
+		pixels.push_back(temp);
+	}
+	
 	// decompress
 	jpeg_start_decompress(&cinfo);
 	
 	// allocate memory
+	row_pointer[0] = (JSAMPLE*)malloc(width * components);
+	printf("WIDTH %i * COMPONENTS %i = %i\n", width, components, width * components);
+	
+	int y = 0, temp = 0;
+	while(cinfo.output_scanline < height)
+	{
+		jpeg_read_scanlines(&cinfo, row_pointer, 1);
+		for (int x = 0; x < width * components; x++)
+		{
+			temp = row_pointer[0][x];
+			pixels[y].push_back(temp);
+		}
+		y++;
+	}
+	
+	for (int i = 0; i < width * components; i++)
+	{
+		row_pointer[0][i] = 0;
+	}
 	
 	// finish decompression, fee memory
 	jpeg_finish_decompress(&cinfo);
 	jpeg_destroy_decompress(&cinfo);
-	fclose(img);
+	fclose(image);
 	free(row_pointer[0]);
-	imageHash[_filename] = ImageColor();
+
+}
+
+ImageColor ImageAnalyzer::FindAvg() {
+	int R = 0, G = 0, B = 0;
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			R += pixels[y][x * components];
+			G += pixels[y][x * components + 1];
+			B += pixels[y][x * components + 2];
+			//printf("(%i, %i)\tR %i\tG %i\tB %i\n", x, y, R, G, B);
+		}
+	}
+	R /= height * width;
+	G /= height * width;
+	B /= height * width;
+	ImageColor color = ImageColor(R, G, B);
+	return color;
+}
+
+void ImageAnalyzer::ProcessImg(char _filename[])
+{
+	ReadImage(_filename);
+	ImageColor avgColor = FindAvg();
+	imageHash[_filename] = avgColor;
 	printf("IMAGE %s READ:\tAVG R %i\tAVG B %i\tAVG G %i\t\n", _filename, imageHash[_filename].R(), imageHash[_filename].B(), imageHash[_filename].G());
 }
 
+void ImageAnalyzer::ProcessChunk(coordinates _topLeft, coordinates _bottomRight) {
+	
+}
+
+
+int main() {
+	
+	ImageAnalyzer I;
+	I.ProcessImg("redchecker2.jpg");
+}
--- ImageAnalyzer.h
+++ ImageAnalyzer.h
@@ -1,11 +1,14 @@
 #ifndef IMAGEANALYZER_H 
 #define IMAGEANALYZER_H
 
-#include <vector>
-#include <stdio>
+#include <stdio.h>
 #include <jpeglib.h>
+#include <jerror.h>
+#include <stdlib.h>
+#include <vector>
 #include <string.h>
 #include <math.h>
+#include <map>
 
 #define SUCCESS			0
 #define FILE_NOT_FOUND	-1
@@ -20,29 +23,33 @@ class ImageColor {
 public:
 	ImageColor();
 	ImageColor(int _red, int _blue, int _green);
-	void setColors(int _red, int _blue, int _green);
+	void SetColors(int _red, int _blue, int _green);
 	int R() { return red; }
 	int B() { return blue; }
 	int G() { return green; }
-}
+};
 
 class ColorMatrix {
 	
-}
+};
+
+typedef pair<int, int> coordinates;
 
 class ImageAnalyzer {
 	int width, height, components;
-	vector<vector<ImageColor> > pixels;
+	vector<vector<int> > pixels;
 	int redsum, bluesum, greensum;
-	map<String, ImageColor> imageHash;
+	map<char*, ImageColor> imageHash;
 	
 public:
 	int Height() { return height; }
 	int Width() { return width; }
 	int Components() { return components; }
 	int ReadImage(char _filename[]);
-	//ImageColor GetAvgColor(char _imgname[]);
+	ImageColor FindAvg();
+	void ProcessImg(char _filename[]);
+	void ProcessChunk(coordinates _topLeft, coordinates _bottomRight);
 
-}
+};
 
 #endif
