9da4ec53471f48125637a8c4d072d516a1883475
Bethany McNabb <aggieforchrist2013@tamu.edu>
2012-12-03 21:06:14 -0600

    Added Mary's Functions to Code
    Just need to add the piping!! :D

--- Find_ID.py
+++ Find_ID.py
@@ -7,32 +7,32 @@ import math
 import collections
 import random
 import time
-#import utils
 import json
 import itertools
 from collections import defaultdict
 import pymongo
 from stemming import porter2
-#import tfidf
-#import numpy as np
-#from numpy import unravel_index
 
 
-def return_id():
-    #REPLACE WITH REAL FUNCTION
-    return '8143186204'
+
+def get_id(tag, dblist):
+    data = []
+    for item in dblist:
+        data.append(item)
+    for entry in data:
+        #print entry
+        if tag in entry['keywords']['keywords']:
+            return entry['filename']
+        #else:
+            #print 'NNNNNNNNOOOOOOOOOOOOO'
+            #return 0
+
+        
+def get_sim(query, stored):
+    diff = [pow((a-b),2) for a,b in zip(query,stored)]
+    return math.sqrt(sum(diff))
 
 class FindID(object):
-    def __init__(self):
-        pass
-
-    #REPLACE WITH REAL FUNCTION
-    def mary_function(self, s1, s2):
-        value1 = math.fabs(s1[0] - s2[0])
-        value2 = math.fabs(s1[1] - s2[1])
-        value3 = math.fabs(s1[2] - s2[2])
-        output = (value1 + value2 + value3)/3
-        return output
 
     def find_clusters(self, id, clusters_inv):
         k = 0
@@ -40,11 +40,9 @@ class FindID(object):
         clust_inv = {}
         for item in clusters_inv:
             clust_inv.update(item)
-        print clust_inv
         while k < len(clust_inv)-1:
             ids.append(clust_inv['tier'+str(k)][str(id)])
             k = k +1
-        print ids
         return ids
 
     def findID(self, cluster_ids, clusters, pics, color1):
@@ -57,16 +55,14 @@ class FindID(object):
         clust = {}
         for item in clusters:
             clust.update(item)
-        print clust
         matching_id = '0'
         for cluster_id in cluster_ids:
-            print 'tier'+str(k)
             pic_ids = clust['tier'+str(k)][str(cluster_id)]
             for id in pic_ids:
                 color2 = pic_data[id]['color']
-                sim = self.mary_function(color1, color2)
+                sim = get_sim(color1, color2)
                 print sim
-                if sim<30:
+                if sim<103:
                     bool = 'FALSE'
                     matching_id = id
                     break
@@ -78,13 +74,16 @@ class FindID(object):
 
 def main():
     mongo = pymongo.Connection('localhost', 27017)
+    merge = mongo['my_database']['merged_info'].find()
     pics = mongo['my_database']['pics'].find()
     clusters = mongo['my_database']['clusters'].find()
     clusters_inv = mongo['my_database']['clusters_inv'].find()
     find_id = FindID()
-    id = return_id()
-    cluster_ids = find_id.find_clusters(id, clusters_inv)
+    ####INPUT RAW INPUT HERE FOR COLOR AND STRING????#####
     color = [102, 9, 39]
+    tag = 'dog'
+    id = get_id(tag, merge)
+    cluster_ids = find_id.find_clusters(id, clusters_inv)
     new_id = find_id.findID(cluster_ids, clusters, pics, color)
     print new_id
     
